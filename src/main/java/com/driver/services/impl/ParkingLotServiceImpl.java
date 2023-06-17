4ea2b73c438e3ac75d17588da8bb1abe6468c3a4
vaishali-sharma <iitmgwl1975@gmail.com>
2023-06-17 16:46:08 +0530

    [models added]

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,6 +1,7 @@
 package com.driver.services.impl;
 
 import com.driver.model.SpotType;
+import com.driver.model.ParkingLot;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
 import com.driver.services.ParkingLotService;
@@ -18,7 +19,12 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
-
+          ParkingLot parkingLot=new ParkingLot();
+          parkingLot.setName(name);
+          parkingLot.setAddress(address);
+          parkingLot.setSpotList(null);
+          parkingLotRepository1.save(parkingLot);
+          return parkingLot;     
     }
 
     @Override
@@ -28,7 +34,7 @@ public class ParkingLotServiceImpl implements ParkingLotService {
 
     @Override
     public void deleteSpot(int spotId) {
-
+       
     }
 
     @Override
@@ -38,6 +44,6 @@ public class ParkingLotServiceImpl implements ParkingLotService {
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
-
+    	parkingLotRepository1.deleteById(parkingLotId);
     }
 }
