32d7824c14aca6612897f882d979e6bdad654fc0
vaishali-sharma <iitmgwl1975@gmail.com>
2023-06-17 22:26:56 +0530

    [reservation done]

--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -1,6 +1,10 @@
 package com.driver.controllers;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Payment;
 import com.driver.model.Reservation;
+import com.driver.model.Spot;
+import com.driver.model.User;
 import com.driver.services.impl.ReservationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+import java.util.List;
 @RestController
 @RequestMapping("/reservation")
 public class ReservationController {
@@ -19,6 +25,41 @@ public class ReservationController {
         //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
         //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
 
-        return null;
+    	 ParkingLot parkingLot = reservationService.findbyId(parkingLotId);
+    	    if (parkingLot == null) {
+    	        throw new Exception("Parking lot not found");
+    	    }
+
+    	    // Check if the user exists
+    	    User user = reservationService.getUserById(userId);
+    	    if (user == null) {
+    	        throw new Exception("User not found");
+    	    }
+
+    	    // Find available spots that match the vehicle's type
+    	    List<Spot> availableSpots = reservationService.getAvailableSpotsByVehicleType(parkingLotId, numberOfWheels);
+
+    	    // Sort the available spots based on the price per hour
+    	    availableSpots.sort(Comparator.comparingDouble(Spot::getPricePerHour));
+
+    	    // Check if any spot is available
+    	    if (availableSpots.isEmpty()) {
+    	        throw new Exception("No spot available for the given vehicle type");
+    	    }
+
+    	    // Reserve the spot with the lowest price per hour
+    	    Spot reservedSpot = availableSpots.get(0);
+    	    int totalPrice = reservedSpot.getPricePerHour() * timeInHours;
+    	    reservedSpot.setPricePerHour(totalPrice);
+
+    	    // Create and save the reservation
+    	    Reservation reservation = new Reservation();
+    	    reservation.setNumberOfHours(timeInHours);
+    	    reservation.setSpot(reservedSpot);
+    	    reservation.setUser(user);
+    	    //reservation.setPayment(null);
+    	    reservationService.saveReservation(reservation);
+
+    	    return reservation;
     }
 }
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -9,6 +9,7 @@ import com.driver.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,4 +27,55 @@ public class ReservationServiceImpl implements ReservationService {
 
     	return null;
     }
+	public User getUserById(Integer userId) {
+		return userRepository3.findById(userId).get();
+	}
+	public void saveReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		reservationRepository3.save(reservation);
+		
+	}
+	public ParkingLot findbyId(Integer parkingLotId) {
+		return parkingLotRepository3.findById(parkingLotId).get();
+	}
+	public List<Spot> getAvailableSpotsByVehicleType(Integer parkingLotId,
+			Integer numberOfWheels) {
+		ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+	    if (parkingLot == null) {
+	        throw new IllegalArgumentException("Parking lot not found");
+	    }
+
+	    List<Spot> availableSpots = new ArrayList<>();
+
+	    // Iterate over the spots in the parking lot
+	    for (Spot spot : parkingLot.getSpotList()) {
+	        // Check if the spot's type is equal to or larger than the vehicle's type
+	        if (check(spot.getSpotType(),numberOfWheels)) {
+	            // Check if the spot is available
+	            if (isSpotAvailable(spot)) {
+	                availableSpots.add(spot);
+	            }
+	        }
+	    }
+
+	    return availableSpots;
+	}
+	private boolean isSpotAvailable(Spot spot) {
+	    // Implement your logic to check if the spot is available
+	    // For example, you could check if the spot is occupied or if it has any reservations during the requested time
+	    // Return true if the spot is available, false otherwise
+	    return !spot.isOccupied();
+	}
+	private boolean check(SpotType spotType,Integer numberOfWheels)
+	{
+		if(spotType.equals("TWO_WHEELER") && numberOfWheels<=2)
+		{
+			return true;
+		}
+		if(spotType.equals("FOUR_WHEELER") && numberOfWheels<=4)
+		{
+			return true;
+		}
+		return false;
+	}
 }
