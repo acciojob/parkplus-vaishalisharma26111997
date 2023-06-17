bc85b0e3a688567c292708163cd6ecfc7d3b95c9
vaishali-sharma <iitmgwl1975@gmail.com>
2023-06-17 23:03:26 +0530

    [payment implement]

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -7,7 +7,7 @@ import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.driver.model.*;
 @Service
 public class PaymentServiceImpl implements PaymentService {
     @Autowired
@@ -18,6 +18,31 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
 
-    	return null;
+    	 Reservation reservation = reservationRepository2.findById(reservationId).get();
+    	    if (reservation == null) {
+    	        throw new IllegalArgumentException("Reservation not found");
+    	    }
+
+    	    // Check if the amount sent is less than the total price
+    	    Payment totalPrice = reservation.getPayment();
+    	    if (amountSent < Integer.BYTES) {
+    	        throw new Exception("Insufficient Amount");
+    	    }
+
+    	    // Normalize the payment mode to lowercase
+    	    String normalizedMode = mode.toLowerCase();
+
+    	    // Check if the payment mode is valid
+    	    if (!normalizedMode.equals("cash") && !normalizedMode.equals("card") && !normalizedMode.equals("upi")) {
+    	        throw new Exception("Payment mode not detected");
+    	    }
+
+    	    // Process the payment using the PaymentService
+    	   
+    	    PaymentService paymentService=null;
+			Payment payment = paymentService.pay(reservationId, amountSent, normalizedMode);
+			 paymentRepository2.save(payment);
+    	    // Return the payment object
+    	    return  payment;
     }
 }
